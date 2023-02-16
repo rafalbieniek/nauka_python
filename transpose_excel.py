e34b2f1abd83d3499d707ed2021768a9018dc798
Rafał Bieniek <rafalbieniek1985@outlook.com>
2023-02-16 12:59:10 +0100

    Odwraca kolejność współrzędnych komórek w Excelu, transpozycja

--- transpose_excel.py
+++ transpose_excel.py
@@ -1,14 +1,14 @@
 import openpyxl
 
-wb = openpyxl.load_workbook('warzywa.xlsx')
+wb = openpyxl.load_workbook('vegetables.xlsx')
 sheet = wb['List1']
 
 new_wb = openpyxl.Workbook()
-new_sheet = new_wb.create_sheet(title='zmieniony', index=0)
+new_sheet = new_wb.create_sheet(title='changed', index=0)
 
 for row in range(1, sheet.max_row + 1):
     for column in range(1, sheet.max_column + 1):
         new_sheet.cell(column, row).value = sheet.cell(row, column).value
 
 
-new_wb.save('warzywa_transpose.xlsx')
+new_wb.save('vegetable_transpose.xlsx')
